fbd6c797abfa5d3c9143b11c9d648869c88cf812
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-30 00:28:32 +0900

    選択中の枠表示を実装

--- Assets/_Project/Scenes/Main/Scripts/BoardView.cs
+++ Assets/_Project/Scenes/Main/Scripts/BoardView.cs
@@ -9,8 +9,14 @@ public class BoardView : MonoBehaviour
     [SerializeField, Tooltip("配置する石の複製元")]
     private DiskView diskOriginal = default;
 
+    [SerializeField, Tooltip("選択中枠の複製元")]
+    private GameObject selectedFrameOriginal = default;
+
     [SerializeField, Tooltip("配置する石の親となるトランスフォーム")]
     private Transform diskParent = default;
+
+    [SerializeField, Tooltip("選択中枠の親となるトランスフォーム")]
+    private Transform selectedFrameParent = default;
 
     [SerializeField, Tooltip("盤の座標系モデル")]
     private BoardCoordinateModel coordinateModel = default;
@@ -20,6 +26,11 @@ public class BoardView : MonoBehaviour
     /// 盤に置かれた石
     /// </summary>
     private DiskView[,] DiskViews { get; } = new DiskView[BoardCoordinateModel.SquareNumberInLine, BoardCoordinateModel.SquareNumberInLine];
+
+    /// <summary>
+    /// 選択中の枠
+    /// </summary>
+    private GameObject SelectedFrame { get; set; }
 
 
     /// <summary>
@@ -55,5 +66,35 @@ public class BoardView : MonoBehaviour
 
         // 指定された座標の石をひっくり返す。
         DiskViews[boardPosition.y, boardPosition.x].Turn();
+    }
+
+    /// <summary>
+    /// 盤のマスを選択します。
+    /// </summary>
+    /// <param name="boardPosition"> 盤上の座標 </param>
+    public void Select(Vector2Int boardPosition)
+    {
+        // 盤上の座標をワールド座標に変換。
+        var worldPosition = coordinateModel.GetWorldPosition(boardPosition);
+
+        // 既に枠があったら指定座標に移動。
+        if (SelectedFrame) {
+            SelectedFrame.transform.position = worldPosition;
+        }
+        // 無かったら指定座標に生成。
+        else {
+            SelectedFrame = Instantiate(selectedFrameOriginal, worldPosition, Quaternion.identity, selectedFrameParent);
+        }
+    }
+
+    /// <summary>
+    /// 盤のマスの選択を解除します。
+    /// </summary>
+    public void Deselect()
+    {
+        if (!SelectedFrame) {
+            throw new Exception("まだマスが選択されていないのに、選択解除しようとしました。");
+        }
+        Destroy(SelectedFrame);
     }
 }
