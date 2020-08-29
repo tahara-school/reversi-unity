fc7c617a836305a46b3276405f2fe7ff93fb6a6d
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-29 22:30:19 +0900

    石のビュークラスを追加

---
+++ Assets/_Project/Scenes/Main/Scripts/DiskView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// オセロの石ビュー
+/// </summary>
+public class DiskView : MonoBehaviour
+{
+    [SerializeField, Tooltip("オセロの石を表す3Dモデル")]
+    private Transform disk = default;
+
+
+    /// <summary>
+    /// ひっくり返ります。
+    /// </summary>
+    /// <param name="isBlack"> 表面は黒か </param>
+    public void Turn(bool isBlack)
+    {
+        if (isBlack) {
+            disk.rotation = Quaternion.identity;
+        }
+        else {
+            disk.rotation = Quaternion.Euler(180f, 0f, 0f);
+        }
+    }
+}
