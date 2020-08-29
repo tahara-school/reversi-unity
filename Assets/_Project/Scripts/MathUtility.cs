753f63283ff1e99d1e34045ac13b8674c73b453c
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-29 22:29:28 +0900

    整数ベクトルが範囲内かを取得する関数を追加

---
+++ Assets/_Project/Scripts/MathUtility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 数学の便利関数群
+/// </summary>
+public static class MathUtility
+{
+    /// <summary>
+    /// 任意の座標が範囲内かを取得します。
+    /// </summary>
+    /// <param name="source"> 任意の座標 </param>
+    /// <param name="min"> 最小値 </param>
+    /// <param name="max"> 最大値 </param>
+    /// <returns> 任意の座標が範囲内か </returns>
+    public static bool IsInRange(Vector2Int source, Vector2Int min, Vector2Int max)
+    {
+        // 範囲内に丸めた座標を用意。
+        var temp = source;
+        temp.Clamp(min, max);
+        // 「元の座標」と「範囲内に丸めた座標」が等しい場合、範囲内である。
+        return source == temp;
+    }
+}
