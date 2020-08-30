27760bea0470300719af396d166b483b97eb14d4
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-30 15:15:28 +0900

    オセロのマスの状態列挙型を追加

---
+++ Assets/_Project/Scenes/Main/Scripts/SquareState.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// マスの状態列挙型
+/// </summary>
+public enum SquareState
+{
+    /// <summary>
+    /// 石が置かれていない
+    /// </summary>
+    Empty,
+
+    /// <summary>
+    /// 黒石が置かれている
+    /// </summary>
+    Black,
+
+    /// <summary>
+    /// 白石が置かれている
+    /// </summary>
+    White,
+}
