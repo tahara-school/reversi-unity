8402157590c7b1e043df977ba65227c1b272b89d
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-30 20:49:22 +0900

    オセロの盤の状態列挙型

---
+++ Assets/_Project/Scenes/Main/Scripts/BoardState.cs
@@ -0,0 +1,20 @@
+﻿/// <summary>
+/// オセロの盤の状態列挙型
+/// </summary>
+public enum BoardState
+{
+    /// <summary>
+    /// 石が置ける
+    /// </summary>
+    Continue,
+
+    /// <summary>
+    /// 石が置けない
+    /// </summary>
+    Pass,
+
+    /// <summary>
+    /// 全てのマスが埋まって試合終了
+    /// </summary>
+    Finish,
+}
