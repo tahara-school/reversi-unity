26bff5f8cae1a769e9891fe8c9cfd9b0b5f9034c
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-30 16:35:03 +0900

    ボード読み取りインタフェースを追加

---
+++ Assets/_Project/Scenes/Main/Scripts/IBoardReader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// オセロの盤の情報読み取りインタフェース
+/// </summary>
+public interface IBoardReader
+{
+    /// <summary>
+    /// 任意の盤上の座標が範囲外かを取得します。
+    /// </summary>
+    /// <param name="boardPosition"> 盤上の座標 </param>
+    /// <returns> 任意の盤上の座標が範囲外か </returns>
+    bool GetIsInRange(Vector2Int boardPosition);
+
+    /// <summary>
+    /// 盤の任意のマスの状態を取得します。
+    /// </summary>
+    /// <param name="boardPosition"> 盤上の座標 </param>
+    /// <returns> 盤の任意のマスの状態 </returns>
+    SquareState GetSquareState(Vector2Int boardPosition);
+}
