f343101f4f4e77d3ca6c575b30e628d4b29cafa4
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-30 18:20:51 +0900

    オセロをプレイするキャラクターインタフェースを追加

---
+++ Assets/_Project/Scenes/Main/Scripts/Character/ICharacter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+/// <summary>
+/// オセロをプレイするキャラクターインタフェース(プレイヤー・通信相手・AIだったり)
+/// </summary>
+public interface ICharacter
+{
+    /// <summary>
+    /// 石を置く場所を取得します。
+    /// </summary>
+    /// <param name="board"> 盤の情報取得インタフェース </param>
+    /// <param name="isBlack"> 置く石の色 </param>
+    /// <returns> 石を置く場所 </returns>
+    UniTask<Vector2Int> GetDiskPutPositionAsync(IBoardReader board, bool isBlack);
+}
