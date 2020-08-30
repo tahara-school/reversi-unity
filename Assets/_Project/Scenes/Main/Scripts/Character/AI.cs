66ee116bbb817a414d80bd831def2a4989b82624
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-30 18:21:32 +0900

    敵AIのキャラクタークラスを追加

---
+++ Assets/_Project/Scenes/Main/Scripts/Character/AI.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+/// <summary>
+/// AIが操作するキャラクター
+/// </summary>
+public class AI : ICharacter
+{
+    /// <summary>
+    /// 石を置く場所を取得します。
+    /// </summary>
+    /// <param name="board"> 盤の情報取得インタフェース </param>
+    /// <param name="isBlack"> 置く石の色 </param>
+    /// <returns> 石を置く場所 </returns>
+    public async UniTask<Vector2Int> GetDiskPutPositionAsync(IBoardReader board, bool isBlack)
+    {
+        // 考えてるふりをする。
+        await UniTask.Delay(System.TimeSpan.FromSeconds(1f));
+
+        // 全マスを走査。
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                var p = new Vector2Int(x, y);
+
+                // 石が既にあるマスは無視。
+                var state = board.GetSquareState(p);
+                if (state != SquareState.Empty) { continue; }
+
+                // 石を置くことで石をひっくり返せるマスだったら、そのマスの座標を返す。
+                var turnDisks = ReversiUtility.GetTurnDisks(board, new DiskInformation(isBlack, p));
+                var turnDisksExist = turnDisks.Count() != 0;
+                if (turnDisksExist) { return p; }
+            }
+        }
+        throw new System.Exception("盤に配置できる場所がありません。");
+    }
+}
