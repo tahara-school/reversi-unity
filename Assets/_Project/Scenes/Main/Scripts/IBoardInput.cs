6b216e06414dfcd72d6c0f3d1e7383ad156d1681
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-30 18:21:56 +0900

    オセロ盤の入力取得インタフェースを追加

---
+++ Assets/_Project/Scenes/Main/Scripts/IBoardInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+/// <summary>
+/// 盤の入力を取得するインタフェース
+/// </summary>
+public interface IBoardInput
+{
+    /// <summary>
+    /// 盤のクリックを待機します。
+    /// </summary>
+    /// <returns> クリックされた盤上の座標 </returns>
+    UniTask<Vector2Int> WaitToClickAsync();
+}
