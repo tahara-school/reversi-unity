36b98c2d1fd385df023cba6117da84af954babe6
Tahara <33447713+Tahara256@users.noreply.github.com>
2020-08-30 17:11:50 +0900

    石の情報構造体を追加

---
+++ Assets/_Project/Scenes/Main/Scripts/DiskInformation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// オセロの石の情報構造体
+/// </summary>
+public struct DiskInformation
+{
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="isBlack"> 黒面か </param>
+    /// <param name="position"> 石の盤上の座標 </param>
+    public DiskInformation(bool isBlack, Vector2Int position)
+    {
+        IsBlack = isBlack;
+        Position = position;
+    }
+
+    /// <summary>
+    /// 黒面か
+    /// </summary>
+    public bool IsBlack { get; set; }
+
+    /// <summary>
+    /// 石の盤上の座標
+    /// </summary>
+    public Vector2Int Position { get; set; }
+}
